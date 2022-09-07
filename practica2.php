d4aa2b063332a16a2865b6cfd4ee956679ad1632
Santi Handel <santiagohandel@gmail.com>
2022-09-06 22:01:09 -0300

    siu

---
+++ practica2.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Formulario de entrada del dato</title>
+</head>
+<body>
+<form method="post" action="pagina2.php">
+Ingrese su nombre:
+<input type="text" name="nombre">
+<br>
+<input type="submit" value="confirmar">
+</form>
+</body>
+</html>
