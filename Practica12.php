97864e932560cbd894c0a2ac57922bfe12e85cac
Santi Handel <santiagohandel@gmail.com>
2022-09-12 10:12:45 -0300

    f

---
+++ Practica12.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$contador=1;
+while($contador<=5){
+    echo $contador*2;
+    $contador++;
+    echo "<br>";
+}
+?>
+</body>
+</html>
---
+++ Practica14.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$f=7;
+$a=1;
+do{
+    echo $a. " multiplicado por 7 es ".$f;
+    echo "<br>";
+    $a++;
+    $f=$f+7;
+
+}while($f<=70);
+?>
+</body>
+</html>
