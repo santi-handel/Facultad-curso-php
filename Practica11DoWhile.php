09b6129e76b8576cc84a791e2d21a78fde8a4e79
Santi Handel <santiagohandel@gmail.com>
2022-09-12 10:02:41 -0300

    siu

---
+++ Practica11DoWhile.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $a=1;
+    $f=2;
+do{
+echo $a." multiplicado por 2 es ".$f;
+echo "<br>";
+$f=$f+2;
+$a=$a+1;
+}while ($f<=20)
+?>
+</body>
+</html>
