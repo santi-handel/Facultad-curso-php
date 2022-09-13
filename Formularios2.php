27a5f74e1b7796caa80afe9ca245920366aaec8e
Santi Handel <santiagohandel@gmail.com>
2022-09-13 08:32:12 -0300

    .

---
+++ Formularios2.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Problema</title>
+</head>
+
+<body>
+<?php
+echo $_REQUEST['nombre'];
+echo "<br>";
+if ($_REQUEST['edad']>=18)
+{
+echo "Es mayor de edad";
+}
+else
+{
+echo "No es mayor de edad";
+}
+?>
+</body>
+</html>
